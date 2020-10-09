accdde2631901c9ce38429d03368e8773af5ce32
Matheus Carvalho <matheuscarvalho.edu@gmail.com>
2020-10-09 13:31:07 -0300

    Finalização aula 18

--- BACK-END/src/main/java/com/desafiodevsuperior/DevSuperior/Resource/CategoryResource.java
+++ BACK-END/src/main/java/com/desafiodevsuperior/DevSuperior/Resource/CategoryResource.java
@@ -1,13 +1,15 @@
-package com.desafiodevsuperior.DevSuperior.Resource;
+package com.desafiodevsuperior.DevSuperior.resource;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.desafiodevsuperior.DevSuperior.dto.CategoryDTO;
 import com.desafiodevsuperior.DevSuperior.entities.Category;
 import com.desafiodevsuperior.DevSuperior.services.CategoryServices;
 
@@ -19,9 +21,15 @@ public class CategoryResource {
 	private CategoryServices service;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> finAll() {
-		List<Category> list = service.finAll();
+	public ResponseEntity<List<CategoryDTO>> finAll() {
+		List<CategoryDTO> list = service.finAll();
 		return ResponseEntity.ok().body(list);
-		
+
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 }
--- BACK-END/src/main/java/com/desafiodevsuperior/DevSuperior/services/CategoryServices.java
+++ BACK-END/src/main/java/com/desafiodevsuperior/DevSuperior/services/CategoryServices.java
@@ -1,22 +1,37 @@
 package com.desafiodevsuperior.DevSuperior.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.desafiodevsuperior.DevSuperior.dto.CategoryDTO;
 import com.desafiodevsuperior.DevSuperior.entities.Category;
 import com.desafiodevsuperior.DevSuperior.repostitories.CategoryRepository;
 
 @Service
 public class CategoryServices {
-	
+
 	@Autowired
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> finAll() {
-		return repository.findAll();
+	public List<CategoryDTO> finAll() {
+		List<Category> list = repository.findAll();
+
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
 	}
 }
